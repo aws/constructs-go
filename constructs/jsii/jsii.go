1adce5f95d089af7087e1ebd6bf2be723f5d71c3
AWS CDK Team <aws-cdk-dev@amazon.com>
2021-02-01 12:24:48 +0000

    chore(release): 3.3.1

---
+++ constructs/jsii/jsii.go
@@ -0,0 +1,17 @@
+package jsii
+
+import (
+	rt "github.com/aws/jsii-runtime-go"
+	"sync"
+)
+
+var once sync.Once
+
+// Initialize performs the necessary work for the enclosing
+// module to be loaded in the jsii kernel.
+func Initialize() {
+	once.Do(func(){
+		// Load this library into the kernel
+		rt.Load("constructs", "3.3.1", tarball)
+	})
+}
