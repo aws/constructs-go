e675498188f25d251dafecd1a93a9af22f681961
AWS CDK Team <aws-cdk-dev@amazon.com>
2021-02-23 05:28:50 +0000

    chore(release): v3.3.35

--- constructs/jsii/jsii.go
+++ constructs/jsii/jsii.go
@@ -12,6 +12,6 @@ var once sync.Once
 func Initialize() {
 	once.Do(func(){
 		// Load this library into the kernel
-		rt.Load("constructs", "3.3.34", tarball)
+		rt.Load("constructs", "3.3.35", tarball)
 	})
 }
