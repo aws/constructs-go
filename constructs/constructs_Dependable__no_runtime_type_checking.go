2afd42bc146dbcd3387fb022d1a4bb6527d6fac0
AWS CDK Team <aws-cdk-dev@amazon.com>
2022-10-11 00:13:18 +0000

    chore(release): v10.1.128

--- constructs/constructs_Dependable__no_runtime_type_checking.go
+++ constructs/constructs_Dependable__no_runtime_type_checking.go
@@ -6,11 +6,15 @@ package constructs
 
 // Building without runtime type checking enabled, so all the below just return nil
 
-func validateConstruct_IsConstructParameters(x interface{}) error {
+func validateDependable_GetParameters(instance IDependable) error {
 	return nil
 }
 
-func validateNewConstructParameters(scope Construct, id *string) error {
+func validateDependable_ImplementParameters(instance IDependable, trait Dependable) error {
+	return nil
+}
+
+func validateDependable_OfParameters(instance IDependable) error {
 	return nil
 }
 
