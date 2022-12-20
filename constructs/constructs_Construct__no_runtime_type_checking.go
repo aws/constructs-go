d2ab211928a50adb4380387f1e86b0b1a7df59fe
AWS CDK Team <aws-cdk-dev@amazon.com>
2022-12-20 00:18:18 +0000

    chore(release): v3.4.190

--- constructs/constructs_Construct__no_runtime_type_checking.go
+++ constructs/constructs_Construct__no_runtime_type_checking.go
@@ -1,15 +1,15 @@
 //go:build no_runtime_type_checking
 
-// A programming model for software-defined state
+// A programming model for composable configuration
 package constructs
 
 // Building without runtime type checking enabled, so all the below just return nil
 
-func validateConstruct_IsConstructParameters(x interface{}) error {
+func (c *jsiiProxy_Construct) validateOnSynthesizeParameters(session ISynthesisSession) error {
 	return nil
 }
 
-func validateNewConstructParameters(scope Construct, id *string) error {
+func validateNewConstructParameters(scope Construct, id *string, options *ConstructOptions) error {
 	return nil
 }
 
