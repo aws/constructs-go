a71daf723ce7c4bdf330782253b7f70ad953a1b5
AWS CDK Team <aws-cdk-dev@amazon.com>
2023-02-02 00:18:31 +0000

    chore(release): v3.4.233

--- constructs/constructs_Node__no_runtime_type_checking.go
+++ constructs/constructs_Node__no_runtime_type_checking.go
@@ -1,15 +1,31 @@
 //go:build no_runtime_type_checking
 
-// A programming model for software-defined state
+// A programming model for composable configuration
 package constructs
 
 // Building without runtime type checking enabled, so all the below just return nil
 
-func (n *jsiiProxy_Node) validateAddMetadataParameters(type_ *string, data interface{}, options *MetadataOptions) error {
+func (n *jsiiProxy_Node) validateAddErrorParameters(message *string) error {
+	return nil
+}
+
+func (n *jsiiProxy_Node) validateAddInfoParameters(message *string) error {
+	return nil
+}
+
+func (n *jsiiProxy_Node) validateAddMetadataParameters(type_ *string, data interface{}) error {
 	return nil
 }
 
 func (n *jsiiProxy_Node) validateAddValidationParameters(validation IValidation) error {
+	return nil
+}
+
+func (n *jsiiProxy_Node) validateAddWarningParameters(message *string) error {
+	return nil
+}
+
+func (n *jsiiProxy_Node) validateApplyAspectParameters(aspect IAspect) error {
 	return nil
 }
 
@@ -18,6 +34,10 @@ func (n *jsiiProxy_Node) validateFindChildParameters(id *string) error {
 }
 
 func (n *jsiiProxy_Node) validateSetContextParameters(key *string, value interface{}) error {
+	return nil
+}
+
+func (n *jsiiProxy_Node) validateSynthesizeParameters(options *SynthesisOptions) error {
 	return nil
 }
 
