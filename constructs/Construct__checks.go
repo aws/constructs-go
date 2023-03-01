f50dcf171e95953f6f95f1bade4ec525ae437c26
AWS CDK Team <aws-cdk-dev@amazon.com>
2023-03-01 00:10:33 +0000

    chore(release): v10.1.264

--- constructs/Construct__checks.go
+++ constructs/Construct__checks.go
@@ -1,33 +1,27 @@
 //go:build !no_runtime_type_checking
 
-// A programming model for composable configuration
+// A programming model for software-defined state
 package constructs
 
 import (
 	"fmt"
-
-	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
 )
 
-func (c *jsiiProxy_Construct) validateOnSynthesizeParameters(session ISynthesisSession) error {
-	if session == nil {
-		return fmt.Errorf("parameter session is required, but nil was provided")
+func validateConstruct_IsConstructParameters(x interface{}) error {
+	if x == nil {
+		return fmt.Errorf("parameter x is required, but nil was provided")
 	}
 
 	return nil
 }
 
-func validateNewConstructParameters(scope Construct, id *string, options *ConstructOptions) error {
+func validateNewConstructParameters(scope Construct, id *string) error {
 	if scope == nil {
 		return fmt.Errorf("parameter scope is required, but nil was provided")
 	}
 
 	if id == nil {
 		return fmt.Errorf("parameter id is required, but nil was provided")
-	}
-
-	if err := _jsii_.ValidateStruct(options, func() string { return "parameter options" }); err != nil {
-		return err
 	}
 
 	return nil
