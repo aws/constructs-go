d31bb922aec9472209f167ae2c0b6b851344d8e9
AWS CDK Team <aws-cdk-dev@amazon.com>
2023-03-07 00:20:49 +0000

    chore(release): v3.4.265

--- constructs/jsii/jsii.go
+++ constructs/jsii/jsii.go
@@ -10,12 +10,12 @@ import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
 )
 
-//go:embed constructs-10.1.270.tgz
+//go:embed constructs-3.4.265.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
 // The implementation is idempotent (and hence safe to be called over and over).
 func Initialize() {
 	// Load this library into the kernel
-	_jsii_.Load("constructs", "10.1.270", tarball)
+	_jsii_.Load("constructs", "3.4.265", tarball)
 }
