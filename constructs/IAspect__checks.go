f1476a0d7c7ce75dbcb155ad68731a2487310369
AWS CDK Team <aws-cdk-dev@amazon.com>
2023-03-17 00:18:50 +0000

    chore(release): v3.4.275

---
+++ constructs/IAspect__checks.go
@@ -0,0 +1,17 @@
+//go:build !no_runtime_type_checking
+
+// A programming model for composable configuration
+package constructs
+
+import (
+	"fmt"
+)
+
+func (i *jsiiProxy_IAspect) validateVisitParameters(node IConstruct) error {
+	if node == nil {
+		return fmt.Errorf("parameter node is required, but nil was provided")
+	}
+
+	return nil
+}
+
