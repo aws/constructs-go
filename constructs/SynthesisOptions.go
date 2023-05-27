60170c5ab6aaaf0c8fb339e0307e7505fb805a18
AWS CDK Team <aws-cdk-dev@amazon.com>
2023-05-27 00:17:07 +0000

    chore(release): v3.4.308

---
+++ constructs/SynthesisOptions.go
@@ -0,0 +1,13 @@
+package constructs
+
+
+// Options for synthesis.
+type SynthesisOptions struct {
+	// The output directory into which to synthesize the cloud assembly.
+	Outdir *string `field:"required" json:"outdir" yaml:"outdir"`
+	// Additional context passed into the synthesis session object when `construct.synth` is called.
+	SessionContext *map[string]interface{} `field:"optional" json:"sessionContext" yaml:"sessionContext"`
+	// Whether synthesis should skip the validation phase.
+	SkipValidation *bool `field:"optional" json:"skipValidation" yaml:"skipValidation"`
+}
+
