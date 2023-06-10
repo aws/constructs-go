59d814a9af5661dc8d3b65d3c9d62dce12ceb08f
AWS CDK Team <aws-cdk-dev@amazon.com>
2023-06-10 00:18:12 +0000

    chore(release): v3.4.322

--- constructs/Node__no_checks.go
+++ constructs/Node__no_checks.go
@@ -4,7 +4,15 @@ package constructs
 
 // Building without runtime type checking enabled, so all the below just return nil
 
-func (n *jsiiProxy_Node) validateAddMetadataParameters(type_ *string, data interface{}, options *MetadataOptions) error {
+func (n *jsiiProxy_Node) validateAddErrorParameters(message *string) error {
+	return nil
+}
+
+func (n *jsiiProxy_Node) validateAddInfoParameters(message *string) error {
+	return nil
+}
+
+func (n *jsiiProxy_Node) validateAddMetadataParameters(type_ *string, data interface{}) error {
 	return nil
 }
 
@@ -12,15 +20,23 @@ func (n *jsiiProxy_Node) validateAddValidationParameters(validation IValidation)
 	return nil
 }
 
+func (n *jsiiProxy_Node) validateAddWarningParameters(message *string) error {
+	return nil
+}
+
+func (n *jsiiProxy_Node) validateApplyAspectParameters(aspect IAspect) error {
+	return nil
+}
+
 func (n *jsiiProxy_Node) validateFindChildParameters(id *string) error {
 	return nil
 }
 
-func (n *jsiiProxy_Node) validateGetContextParameters(key *string) error {
+func (n *jsiiProxy_Node) validateSetContextParameters(key *string, value interface{}) error {
 	return nil
 }
 
-func (n *jsiiProxy_Node) validateSetContextParameters(key *string, value interface{}) error {
+func (n *jsiiProxy_Node) validateSynthesizeParameters(options *SynthesisOptions) error {
 	return nil
 }
 
