83cad82e0af936a2d13785d197dabf6a0e0338a0
AWS CDK Team <aws-cdk-dev@amazon.com>
2023-06-12 00:10:53 +0000

    chore(release): v10.2.50

--- constructs/ConstructOptions.go
+++
@@ -1,9 +0,0 @@
-package constructs
-
-
-// Options for creating constructs.
-type ConstructOptions struct {
-	// A factory for attaching `Node`s to the construct.
-	NodeFactory INodeFactory `field:"optional" json:"nodeFactory" yaml:"nodeFactory"`
-}
-
